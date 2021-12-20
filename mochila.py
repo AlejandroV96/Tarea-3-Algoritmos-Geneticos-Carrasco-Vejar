98d1c696990b4dbc0a08dff724905257be834906
sessionmaestra <70710866+sessionmaestra@users.noreply.github.com>
2021-12-19 22:36:59 -0300

    lectura datos

--- mochila.py
+++ mochila.py
@@ -10,18 +10,26 @@ if (len(sys.argv)==5 or len(sys.argv)==2):
                     tau = 1.4
                     ite = 100
                     entrada = sys.argv[1]
-                    print(' --- Valores ---\nSemilla: ', semilla,'\nTau: ', tau,'\nIteraciones: ', ite,'\nArchivo: ', entrada)         
+                    print(' <--- Valores --->\nSemilla: ', semilla,'\nTau: ', tau,'\nIteraciones: ', ite,'\nArchivo: ', entrada)         
           if len(sys.argv)==5:
                     semilla = int(sys.argv[1])
                     tau = float(sys.argv[2])
                     ite = int(sys.argv[3])
                     entrada = sys.argv[4]
-                    print(' --- Valores ---\nSemilla: ', semilla,'\nTau: ', tau,'\nIteraciones: ', ite,'\nArchivo: ', entrada)        
+                    print(' <--- Valores --->\nSemilla: ', semilla,'\nTau: ', tau,'\nIteraciones: ', ite,'\nArchivo: ', entrada)        
 else:
           print("Error al ingresar los parametros.")
           sys.exit(0)
+
 tiempo_proceso_ini = time.process_time()
 np.random.seed(semilla)
 
-df = pd.read_csv(entrada,sep='\t',header=None)
-print(df)
+data = pd.read_csv(entrada,sep='\t',header=None).to_numpy()
+nombreArchivo = data[0][0]
+cantidadElementos = data[1][1]
+capacidadMochila = data[2][1]
+valorOptimo = data[3][1]
+data = pd.read_csv(entrada,sep='\t',header=None, skiprows=5, skipfooter=1, engine='python')
+data = data.drop(columns=0, axis=1).to_numpy()
+print(data)
+print(' <---Datos leidos --->\nNombre Archivo: ', nombreArchivo,'\nCantidad elementos: ', cantidadElementos,'\nCapacidad mochila: ', capacidadMochila,'\nValor optimo: ',valorOptimo)
